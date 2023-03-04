17faf2678bdcd5c012f21d138280cef0005cbdeb
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-04 14:57:16 -0500

    working search and tag api

--- controllers/LastFmController.js
+++ controllers/LastFmController.js
@@ -1,26 +1,53 @@
-import axios from 'axios'
-require('dotenv').config
-const apiKey = process.env.REACT_APP_API_KEY
+const axios = require('axios')
+require('dotenv').config()
+const API_KEY = process.env.API_KEY
 
-export const GetTopAlbumsByTag = async (req, res) => {
+const GetTopAlbumsByTag = async (req, res) => {
   try {
     const { tag } = req.params
     const response = await axios.get(
-      `http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${tag}&api_key=${apiKey}&format=json`
+      `http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${tag}&api_key=${API_KEY}&format=json`
     )
-    return res.status(200).json(response.data.results.albummatches.album)
+    const resultArray = response.data.albums.album
+    console.log(response)
+    const responseArray = []
+
+    resultArray.forEach((album) => {
+      responseArray.push({
+        albumName: album.name,
+        mbid: album.mbid,
+        artist: album.artist,
+        image_url: album.image[2]
+      })
+    })
+    return res.status(200).json(responseArray)
   } catch (error) {
     throw error
   }
 }
-export const SearchAlbumsByName = async (req, res) => {
+const SearchAlbumsByName = async (req, res) => {
   try {
     const { name } = req.params
     const response = await axios.get(
-      `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${name}&api_key=${apiKey}&format=json`
+      `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${name}&api_key=${API_KEY}&format=json`
     )
-    return res.status(200).json(response.data.results.albummatches.album)
+    const resultArray = response.data.results.albummatches.album
+    const responseArray = []
+
+    resultArray.forEach((album) => {
+      responseArray.push({
+        albumName: album.name,
+        mbid: album.mbid,
+        artist: album.artist,
+        image_url: album.image[2]
+      })
+    })
+    return res.status(200).json(responseArray)
   } catch (error) {
     throw error
   }
+}
+module.exports = {
+  GetTopAlbumsByTag,
+  SearchAlbumsByName
 }
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const ReviewRouter = require('./ReviewRouter')
 const AlbumRouter = require('./AlbumRouter')
-const LastFmRouter = require('/LastFmRouter')
+const LastFmRouter = require('./LastFmRouter')
 
 Router.use('/users', UserRouter)
 Router.use('/feed', ReviewRouter)
