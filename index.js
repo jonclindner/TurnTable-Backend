143759c1420ba84176c61071590621bec2a9b73d
Sarah Maher <119453789+mahers12@users.noreply.github.com>
2023-03-06 09:04:10 -0700

    added app router

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api', AppRouter)
 // app.use('/auth', AuthRouter)
 // app.use('/posts', ReviewRouter)
 
