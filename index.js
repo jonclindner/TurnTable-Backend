a4aca86693bc80dc7220165ace93050a2e19e9c1
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-06 09:45:31 -0500

    added api info call

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const logger = require('morgan')
+const AppRouter = require('./routes/AppRouter')
+
+// const AuthRouter = require('./routes/AuthRouter')
+const PostRouter = require('./routes/PostRouter')
 
 const app = express()
-
-const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// app.use('/auth', AuthRouter)
+app.use('/posts', PostRouter)
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api', AppRouter)
+app.get('/', (req, res) => {
+  res.send('This is the base path!')
+})
 
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server Running On Port ${PORT} . . . `))
