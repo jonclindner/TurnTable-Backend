f73f0224b2d6408de0489212176ac64bb70f4f6e
Sarah Maher <119453789+mahers12@users.noreply.github.com>
2023-03-06 08:04:33 -0700

    merged post and review routes and controllers

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -1,10 +1,26 @@
 const Router = require('express').Router()
 const controller = require('../controllers/ReviewController')
+const middleware = require('../middleware')
 
 Router.get('/:review', controller.GetReviews)
-Router.get('/:review_id', controller.GetReviewDetails)
-Router.post('/:user_id', controller.CreateReview)
-Router.put('/:review_id', controller.UpdateReview)
-Router.delete('/:review_id', controller.DeleteReview)
+Router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateReview
+)
+
+Router.put(
+  '/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateReview
+)
+Router.delete(
+  '/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteReview
+)
 
 module.exports = Router
