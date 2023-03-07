d47a2296c5f92803d1585c6759a56791c8c16198
Sarah Maher <119453789+mahers12@users.noreply.github.com>
2023-03-06 20:54:31 -0700

    deleted user router and controller

--- routes/UserRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router()
-const controller = require('../controllers/UserController')
-
-Router.get('/', controller.GetUsers)
-Router.get('/:user_id', controller.GetUserProfile)
-
-module.exports = Router
