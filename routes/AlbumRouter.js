0f566e782a27cf8d1c14b47c5d1a75f756989646
jonclindner <jonathan.c.lindner@gmail.com>
2023-03-08 15:46:27 -0700

    pre pull commit

--- routes/AlbumRouter.js
+++ routes/AlbumRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/AlbumController')
 
 Router.post('/create-album', controller.CreateAlbum)
 Router.get('/get-albums', controller.GetAlbums)
-Router.get('/get-album-details/:album_id', controller.GetAlbumDetails)
+
 Router.get(
   '/get-album-by-name-and-artist/:artist/:name',
   controller.GetAlbumByNameAndArtist
